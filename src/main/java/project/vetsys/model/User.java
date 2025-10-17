348208823918be7a0e41e10365598d2cee6b1c67
ospina27 <alejandro.ospina.lopez@correounivalle.edu.co>
2025-10-16 23:50:55 -0500

    user

--- src/main/java/project/vetsys/model/User.java
+++ src/main/java/project/vetsys/model/User.java
@@ -10,4 +10,70 @@ package project.vetsys.model;
  */
 public class User {
     
+    private int id;
+    private int clinicId;
+    private int roleId;
+    private int statusId;
+    private String username;
+    private String password;
+
+    public User(int id, int clinicId, int roleId, int statusId, String username, String password) {
+        this.id = id;
+        this.clinicId = clinicId;
+        this.roleId = roleId;
+        this.statusId = statusId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClinicId() {
+        return clinicId;
+    }
+
+    public void setClinicId(int clinicId) {
+        this.clinicId = clinicId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
 }
