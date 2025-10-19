c39d19086ef7d463b4a3bccf9b8ad21bf14796b0
Yully05 <yully.velandia@correounivalle.edu.co>
2025-10-18 23:42:15 -0500

    validacion de login segun usuario, contraseña y estado activo. Entrada de datos por consola, pendiente la vista.

--- src/main/java/project/vetsys/VetSys.java
+++ src/main/java/project/vetsys/VetSys.java
@@ -5,7 +5,10 @@
 package project.vetsys;
 
 import java.sql.Connection;
+import java.util.Scanner;
+import project.vetsys.dao.UserDAO;
 import project.vetsys.database.DBConnection;
+import project.vetsys.model.User;
 
 /**
  *
@@ -14,7 +17,24 @@ import project.vetsys.database.DBConnection;
 public class VetSys {
 
     public static void main(String[] args) {
+        
         Connection connection = DBConnection.getConnection();
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Usuario: ");
+        String username = scanner.nextLine();
+        System.out.print("Contraseña: ");
+        String password = scanner.nextLine();
+        
+        UserDAO dao = new UserDAO();
+        User user = dao.login(username, password);
+
+        if (user != null) {
+            System.out.println("Login exitoso: " + user.getUsername());
+            System.out.println(user);
+        } else {
+            System.out.println("Login no exitoso.");
+        }
         
     }
 }
--- src/main/java/project/vetsys/model/User.java
+++ src/main/java/project/vetsys/model/User.java
@@ -10,4 +10,111 @@ package project.vetsys.model;
  */
 public class User {
     
+    private int id_user;
+    private int id_clinic;
+    private int id_role;
+    private int id_status;    
+    private String username;
+    private String password;
+    private String name_clinic;
+    private String name_role;
+    private String name_status;
+    
+    public User() {
+        
+    }
+
+    public User(int id, String clinic, String role, String status, String username, String password) {
+        this.id_user = id;
+        this.name_clinic = clinic;
+        this.name_role = role;
+        this.name_status = status;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public int getId_clinic() {
+        return id_clinic;
+    }
+
+    public int getId_role() {
+        return id_role;
+    }
+
+    public int getId_status() {
+        return id_status;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName_clinic() {
+        return name_clinic;
+    }
+
+    public String getName_role() {
+        return name_role;
+    }
+
+    public String getName_status() {
+        return name_status;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public void setId_clinic(int id_clinic) {
+        this.id_clinic = id_clinic;
+    }
+
+    public void setId_role(int id_role) {
+        this.id_role = id_role;
+    }
+
+    public void setId_status(int id_status) {
+        this.id_status = id_status;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName_clinic(String name_clinic) {
+        this.name_clinic = name_clinic;
+    }
+
+    public void setName_role(String name_rol) {
+        this.name_role = name_rol;
+    }
+
+    public void setName_status(String name_status) {
+        this.name_status = name_status;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: " + username + ", clinica: " + name_clinic + ", rol: " + name_role + ", estado: " + name_status;
+    }
+
+    
+
+    
+    
+    
+    
+    
 }
