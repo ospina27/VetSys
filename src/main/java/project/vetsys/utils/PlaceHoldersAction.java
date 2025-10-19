ff9f0d42652d21d8c5b09128aaf3a8159b88df87
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-10-19 16:16:11 -0500

    Modification of the PlaceHolder Class

--- src/main/java/project/vetsys/utils/PlaceHoldersAction.java
+++ src/main/java/project/vetsys/utils/PlaceHoldersAction.java
@@ -1,4 +1,30 @@
 package project.vetsys.utils;
 
-public class PlaceHoldersAction {
+import javax.swing.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class PlaceHoldersAction implements FocusListener {
+    protected final JTextField inField;
+    protected final String placeholderMsg;
+
+    public PlaceHoldersAction(JTextField inField, String placeholderMsg) {
+        this.inField = inField;
+        this.placeholderMsg = placeholderMsg;
+        this.inField.setText(placeholderMsg);
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (inField.getText().equals(placeholderMsg)) {
+            inField.setText("");
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (inField.getText().isEmpty()) {
+            inField.setText(placeholderMsg);
+        }
+    }
 }
