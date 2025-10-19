23e08a9f275d1fdd9c62fd351e118b22cd29eece
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-10-19 15:22:23 -0500

    Creating the panel init

--- src/main/java/project/vetsys/view/InitPanel.java
+++ src/main/java/project/vetsys/view/InitPanel.java
@@ -1,7 +1,21 @@
 package project.vetsys.view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class InitPanel {
+public class InitPanel{
     private JPanel main;
+    private JLabel InitPanel_lblTitleWelcome;
+    private JButton InitPanel_BttnEnter;
+    private JLabel InitPanel_Icon;
+
+    public InitPanel(){
+
+        this.InitPanel_BttnEnter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                //
+            }
+        })
+    }
 }
