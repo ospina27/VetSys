b624bd4a2cb6061a1c15664681b559e971da7a3e
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-10-20 07:16:36 -0500

    Conection of Menu Manager from of views

--- src/main/java/project/vetsys/utils/PanelsMap.java
+++ src/main/java/project/vetsys/utils/PanelsMap.java
@@ -4,13 +4,17 @@ import project.vetsys.view.*;
 import project.vetsys.view.InitPanel;
 import project.vetsys.view.MainPanel;
 import project.vetsys.view.LogInPanel;
+import project.vetsys.view.manager.MenuManagerPanel;
 
 import javax.swing.JPanel;
 
 enum PanelsMap {
 
     WELCOME("Welcome", new InitPanel().getPanel()),
-    LOGIN("Login", new LogInPanel().getPanel());
+    LOGIN("Login", new LogInPanel().getPanel()),
+
+    //Templates for Manager
+    MENU_MANAGER("Menu_Manager", new MenuManagerPanel().getPanel());
 
 
     private final String name;
--- src/main/java/project/vetsys/view/manager/MenuManagerPanel.java
+++ src/main/java/project/vetsys/view/manager/MenuManagerPanel.java
@@ -1,11 +1,26 @@
 package project.vetsys.view.manager;
 
-import javax.swing.*;
+import project.vetsys.utils.AccessPanel;
 
-public class MenuManagerPanel {
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class MenuManagerPanel implements AccessPanel{
     private JPanel MenuManagerPanelMain;
     private JLabel MenuManager_lblTitle;
     private JButton MenuManager_BttnOption1;
     private JButton MenuManager_BttnLogOut;
     private JButton MenuManager_BttnOption2;
+
+    public MenuManagerPanel() {
+        this.MenuManager_BttnOption1.addActionListener(e -> AccessPanel.changeContent("Welcome"));
+        this.MenuManager_BttnOption2.addActionListener(e -> AccessPanel.changeContent("Welcome"));
+        this.MenuManager_BttnLogOut.addActionListener(e -> AccessPanel.changeContent("Login"));
+    }
+
+
+    @Override
+    public JPanel getPanel() {
+        return MenuManagerPanelMain;
+    }
 }
