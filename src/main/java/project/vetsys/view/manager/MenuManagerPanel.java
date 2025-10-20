eca900568fa162d693ac33781081c4f2d9c62265
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2025-10-20 07:04:56 -0500

    Creation of Menu Manager View

---
+++ src/main/java/project/vetsys/view/manager/MenuManagerPanel.java
@@ -0,0 +1,11 @@
+package project.vetsys.view.manager;
+
+import javax.swing.*;
+
+public class MenuManagerPanel {
+    private JPanel MenuManagerPanelMain;
+    private JLabel MenuManager_lblTitle;
+    private JButton MenuManager_BttnOption1;
+    private JButton MenuManager_BttnLogOut;
+    private JButton MenuManager_BttnOption2;
+}
