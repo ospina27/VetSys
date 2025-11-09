8fa52d3182a9cc091e10620fb539478707acc300
ospina27 <alejandro.ospina.lopez@correounivalle.edu.co>
2025-11-09 18:46:40 -0500

    Incorporación al DAO de status, para validaciones. En la vista se crea una clase de combobox para crear un metodo que llene cada uno

---
+++ src/main/java/project/vetsys/dao/StatusDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.vetsys.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import project.vetsys.database.DBConnection;
+import project.vetsys.model.Status;
+import project.vetsys.model.User;
+
+/**
+ *
+ * @author User
+ */
+public class StatusDAO {
+    
+    public List<Status> getAllStatus(User logUser){
+        List<Status> status = new ArrayList<>();
+        String sql = "Select * FROM estado";
+        try (Connection con = DBConnection.getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                status.add(new Status(
+                    rs.getInt("id_estado"),
+                    rs.getString("nombre")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al obtener estado: " + e.getMessage());
+        }
+        return status;
+    } 
+    
+}
