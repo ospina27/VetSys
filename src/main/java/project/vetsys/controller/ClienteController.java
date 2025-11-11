327f2dcdd9065717468285d4468fac89e266056c
Yully05 <yully.velandia@correounivalle.edu.co>
2025-11-10 20:03:24 -0500

    creacion crud clientes

---
+++ src/main/java/project/vetsys/controller/ClienteController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.vetsys.controller;
+
+import java.util.List;
+import project.vetsys.dao.ClienteDAO;
+import project.vetsys.model.ClienteModel;
+
+/**
+ *
+ * @author Asus
+ */
+public class ClienteController {
+    
+    private ClienteDAO clienteDAO = new ClienteDAO();
+
+    public boolean guardarCliente(ClienteModel c) {
+        return clienteDAO.insertarCliente(c);
+    }
+
+    public List<ClienteModel> obtenerClientes(int idClinica) {
+        return clienteDAO.listarClientes(idClinica);
+    }
+
+    public boolean modificarCliente(ClienteModel client) {
+        return clienteDAO.actualizarCliente(client);
+    }
+
+    public boolean eliminarCliente(int idCliente, int idClinica) {
+        return clienteDAO.eliminarCliente(idCliente, idClinica);
+    }
+    
+    
+    
+}
